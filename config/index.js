6933b06bd166138ea8da65809fee7f52e3192515
minfuf <fumin0613@163.com>
2019-07-29 09:13:51 +0800

    引入my-ip

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path');
-
+const myIP = require('my-ip');
 module.exports = {
   dev: {
 
@@ -23,7 +23,7 @@ module.exports = {
     },
     // Various Dev Server settings
     //host: 'localhost', // can be overwritten by process.env.HOST
-    host: '172.16.2.223', // can be overwritten by process.env.HOST
+    host: myIP(), // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
